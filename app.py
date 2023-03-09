da03964bbb1bdb4a13a228d9d775bdc7dca3b96e
SheTroxel <smtroxel@gmail.com>
2023-03-08 23:36:05 -0500

    updated code and finalized

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-
+#Set up dependencies
+#################################################################################
 import numpy as np
-
+import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-import datetime as dt
 from flask import Flask, jsonify
 
 #############################################################################
-#Database Setup
+#Set database engine for Flask and connect to SQLite database file
 ############################################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-#refect on existing database into a new model
+#refect on existing database into classes
 Base = automap_base()
 #refect the tables
 Base.prepare(engine, reflect = True)
 print(Base.classes.keys())
 
-# Save references to each tables
+# Set class variables
 measurement = Base.classes.measurement
 station = Base.classes.station
 
@@ -37,11 +37,20 @@ def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
+        f"List of precipatation values from all stations for prior year<br/>"
         f"/api/v1.0/station<br/>"
+        f"List of stations with total precipitation values<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"List of temperatures observed<br/>"
+        f"/api/v1.0/temp/<start>"
+        f"When given a start date (YYYY-MM-DD), calculates the MIN/AVG/MAX For a specified start date greater than or equal to the start date.<br/>"
+        f"/api/v1.0/temp_range<start>/<end>"
+        f"When given a start and end date (YYYY-MM-DD), calculates the MIN/AVG/MAX for the start date to the end date, inclusive<br/>"
     )
-
+#/api/v1.0/temp/start/end
+##########################################################################
 #set up first route for preciptiation data values and date
+#########################################################################
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create our session (link) from Python to the DB
@@ -62,8 +71,9 @@ def precipitation():
 
     session.close()
     return jsonify(precip_dictionary)
-
+###################################################################################
 #set up route for all Stations
+###################################################################################
 @app.route("/api/v1.0/station")
 def station():
     # Create our session (link) from Python to the DB
@@ -77,8 +87,9 @@ def station():
     
     session.close()
     return jsonify(active_station)
-
-#set up route for tobs
+##################################################################################################################################################################################
+#set up route for Tobs 
+######################################################################################################
 @app.route("/api/v1.0/tobs")
 def tobs():
     #create our session (link) from Python to the DB
@@ -97,6 +108,40 @@ def tobs():
     
     session.close()
     return jsonify(most_active_station)  
+######################################################################################################
+#set up weather statistics route one (start)
+#############################################################################################################
+@app.route ("/api/v1.0/temp/<start>")
+def stats1 (start ):
+    session = Session(engine)
+    # define time period. Online assist from git hub user davidwjones for this one.
+    start_date =dt.datetime.strptime(start,'%Y-%m-%d')
+    data = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= start).all()
+    stats = list(np.ravel(data))
+
+    session.close()
+    return jsonify(stats)    
+    
+######################################################################################################
+#set up weather statistics route two (start and end date)
+#############################################################################################################                           
+@app.route ("/api/v1.0/temp_range/<start>/<end>")
+def stats2 (start,end):
+    session = Session(engine)
+    # define time period from start to end date
+    # and get MIN/AVG/MAX temperatures from back one year
+    #  Online assist from git hub user davidwjones for this one.
+    start_date =dt.datetime.strptime(start,'%Y-%m-%d')
+    end_date =dt.datetime.strptime(end,'%Y-%m-%d')
+    data = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+        filter(measurement.date >= start_date).filter(measurement.date <= end_date).all()
+    stats2 = list(np.ravel(data))
+
+    print(start_date, end_date, stats2)
+
+    session.close()
+    return jsonify(stats2)  
 
 if __name__ == '__main__':
     app.run(debug=True)
